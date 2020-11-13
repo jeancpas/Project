628afcb67f686b34d0cf1d7177932591895e6744
jeancpas <jeanc.hondo71@gamil.comgit config --global --listgit config --global user.email jeanc.hondo71@gmail.com>
2020-11-13 21:54:38 +0100

    Initial commit

---
+++ resources/views/content/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+@extends('layouts.master')
+<html>
+    <head>
+        <title>Index</title>
+
+    </head>
+    <body>
+       @section('content')
+            <div class="title"> <h1> Henlo </h1>
+            <p> laravel is cool </p>
+            <a href="#"> Click hier </a>
+            <hr>
+            </div>
+       </div>
+       @endsection
+    </body>
+</html>
